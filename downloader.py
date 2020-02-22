a2cbeeb0690534b9c04a467eb4d0569c968dd992
Daniel Zou <zoudl2000@gmail.com>
2020-02-22 12:32:12 -0800

    download single audio files

--- downloader.py
+++ downloader.py
@@ -1,15 +1,27 @@
-import youtube_dl
+import youtube_dl as dl
 import sqlite3
+import sys
 import os
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+M4A_DIR = os.path.join(CURRENT_DIR, 'm4a')
 
 OPTIONS = {
     'format': 'bestaudio/best',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
+        'preferredcodec': 'm4a',
+        'preferredquality': '192'
     }],
+    'hls_prefer_native': True,
+    'outtmpl': os.path.join(M4A_DIR, '%(title)s.%(ext)s'),
+    'forcetitle': True,
+    'forcefilename': True
 }
 
-CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-MP3_DIR = os.path.join(CURRENT_DIR, 'mp3')
+def download(yt_url):
+    with dl.YoutubeDL(OPTIONS) as ydl:
+        ydl.download([yt_url])
+
+if __name__ == "__main__":
+    download('https://www.youtube.com/watch?v=kl4_Z7_0rmA')
