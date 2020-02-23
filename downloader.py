aadcadb8249d6fdad509a1db1202f5e9b9007b17
Daniel Zou <zoudl2000@gmail.com>
2020-02-23 15:53:44 -0800

    minor changes, import argparse

--- downloader.py
+++ downloader.py
@@ -1,6 +1,7 @@
 import youtube_dl as dl
 import sqlite3
 import re
+import argparse
 import sys
 from os import path
 from traceback import print_exc
@@ -18,7 +19,6 @@ OPTIONS = {
         'preferredquality': '192'
     }],
     'hls_prefer_native': True,
-    'nooverwrites': True,
     'outtmpl': path.join(DOWNLOAD_DIR, '%(title)s.%(ext)s'),
     'quiet': True,
     'forcefilename': True
@@ -37,9 +37,12 @@ def download_single(video_id):
                 print(f'Downloading video <{video_id}>...')
                 info_dict = ydl.extract_info(video_id, download=True)
                 title = info_dict.get('title', None)
-                if title is not None:
+                duration = info_dict.get('duration', None)
+
+                if title is not None and duration is not None:
                     filepath = path.join(DOWNLOAD_DIR, f'{title}.{EXT}')
-                    c.execute('INSERT INTO archive VALUES(?, ?)', (video_id, filepath))
+                    c.execute('INSERT INTO archive VALUES(?, ?, ?)', (video_id, filepath, duration))
+
         except Exception:
             print_exc()
     else:
@@ -72,7 +75,7 @@ def download_playlist(list_url, list_id):
 def refresh_archive():
     conn = sqlite3.connect(ARCHIVE_DB)
     c = conn.cursor()
-    c.execute('CREATE TABLE IF NOT EXISTS archive(video_id UNIQUE, filepath)')
+    c.execute('CREATE TABLE IF NOT EXISTS archive(video_id UNIQUE, filepath, duration)')
     c.execute('SELECT filepath FROM archive')
     filepaths = c.fetchall()
 
@@ -82,6 +85,23 @@ def refresh_archive():
                 c.execute('DELETE FROM archive WHERE filepath=?', (f[0],))
     conn.commit()
     conn.close()
+
+
+def archive_info():
+    conn = sqlite3.connect(ARCHIVE_DB)
+    c = conn.cursor()
+    c.execute('SELECT COUNT(*) FROM archive')
+    num_files = c.fetchone()[0]
+    c.execute('SELECT SUM(duration) FROM archive')
+    total_duration = seconds_to_hours(c.fetchone()[0])
+    conn.commit()
+    conn.close()
+
+    info = {
+        'num_files': num_files,
+        'total_duration': total_duration
+    }
+    return info
 
 
 def parse_url(url):
@@ -104,8 +124,19 @@ def parse_url(url):
     return parsed
 
 
+def seconds_to_hours(seconds):
+    hours = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+    return f'{hours}:{minutes}:{seconds}'
+
+
 if __name__ == "__main__":
     refresh_archive()
+
+    parser = argparse.ArgumentParser()
+    # implement parser
 
     if len(sys.argv) > 1:
         url = sys.argv[1]
@@ -123,3 +154,7 @@ if __name__ == "__main__":
             download_playlist(url, list_id)
         else:
             print('URL not recognized, should contain \'watch\' or \'playlist\'')
+
+    info = archive_info()
+    print('Number of files: ' + str(info.get('num_files', None)))
+    print('Total duration: ' + info.get('total_duration', None))
